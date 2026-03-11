ef55d81d60399c779f84f25de2db8d0bd6440096
sonnh-dev <hoangsonnguyen.contact@gmail.com>
2026-03-11 16:54:51 +0700

    first commit
    init the project

---
+++ src/main/java/com/dxlab/gamepromotionweb/GamePromotionWebApplication.java
@@ -0,0 +1,13 @@
+package com.dxlab.gamepromotionweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamePromotionWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GamePromotionWebApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dxlab/gamepromotionweb/GamePromotionWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dxlab.gamepromotionweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GamePromotionWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
